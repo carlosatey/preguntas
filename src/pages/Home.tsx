bdd0b00d81d97e09f0898f1e314d0534499fe667
carlosalberto <carlosalberto@imaginagroup.com>
2024-12-23 17:45:30 +0100

    Aviso de Test Finalizado al concluir con todas las preguntas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,25 @@
-import {Flex, Text} from "@chakra-ui/react"
-import {Formulario} from "../components/Formulario"
+import { Flex, Text } from "@chakra-ui/react"
+import { Formulario } from "../components/Formulario"
 import testQuestions from "../json/testQuestion.json"
 import { DataContext } from "../context/useContext"
 import { useContext } from "react"
 
 const Home = () => {
-    const {numberQuestion} = useContext(DataContext);
+    const totalQuestions = testQuestions.length;
+    const { numberQuestion } = useContext(DataContext);
 
     return (
         <Flex direction="column" align="center" justify="center" w="100vw" h="100vh">
             <Text mb='20px' fontSize='28'>
                 Home-Admin
             </Text>
-            
-            <Formulario question={testQuestions[numberQuestion]}/>
+            {totalQuestions === numberQuestion ? 
+                <Text>Test finalizado</Text> 
+            : 
+            <Formulario question={testQuestions[numberQuestion]} />
+            }
         </Flex>
     )
 }
 
-export {Home}
+export { Home }
